73391002b56a59ba9b34893fde23ff7d7fad6c13
amarbabu <amarbabu@gmail.com>
2022-02-09 14:52:22 +0000

    h

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ class Main {
   public static void main(String[] args) {
     BusinessDaysExamples.main(null);
     String secret = System.getenv("my-secret");
-    System.out.printf("hssshhhh ...! %s\n", secret);
+    System.out.printf("hssshhh ...! %s\n", secret);
   }
 }
+
