97360e96d7ec23abd2beb483c7b6f880bc675a67
amarbabu <amarbabu@gmail.com>
2022-02-09 20:09:35 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+class Main {
+  public static void main(String[] args) {
+    BusinessDaysExamples.main(null);
+    String secret = System.getenv("my-secret");
+    System.out.printf("hssshhhh ...! %s\n", secret);
+  }
+}
